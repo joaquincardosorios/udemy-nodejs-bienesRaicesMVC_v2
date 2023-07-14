721d000b74344fb33494b5006dd858b451b61b89
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-13 20:21:12 -0400

    feat(db): Configuracion de variables de entorno

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 import Sequelize from 'sequelize'
+import dotenv from 'dotenv'
+dotenv.config({path:'.env'})
 
-const db = new Sequelize('proyectoBienesRaices','root','alohomora',{
-    host: '192.168.1.114',
+const db = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASS,{
+    host: process.env.DB_HOST,
     port:3306,
     dialect: 'mysql',
     define:{
