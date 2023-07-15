7e8e58e51299e71ec52aea17347a161d273d0982
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-15 18:42:51 -0400

    feat(modelo): Se crea modelo de propieda, categoria y precio

---
+++ model/Propiedad.js
@@ -0,0 +1,52 @@
+import { DataTypes } from 'sequelize'
+import db from '../config/db.js'
+
+
+const Propiedad = db.define('propiedades',{
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKeu: true
+    },
+    titulo:{
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    descripcion: {
+        type: DataTypes.TEXT,
+    }, 
+    habitaciones: {
+        type: DataTypes.INTEGER
+    },
+    estacionamiento: {
+        type: DataTypes.INTEGER
+    },
+    wc: {
+        type: DataTypes.INTEGER
+    },
+    calle:{
+        type: DataTypes.STRING(60),
+        allowNull: false
+    },
+    lat:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    lng:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    imagen:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    publicado:{
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+
+})
+
+export default Propiedad;
