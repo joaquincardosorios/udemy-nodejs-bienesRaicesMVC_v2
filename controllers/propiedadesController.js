ef16d2dce3dd0268288823880dfa5e2bd8103feb
Joaquin Rios <joaquin.rios@crecic.cl>
2023-07-17 11:15:31 -0400

    feat(propiedades): Validar agregar imagen

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -75,6 +75,26 @@ const guardar = async (req,res) => {
 }
 
 const agregarImagen = async (req,res) => {
+
+    const { id } = req.params
+
+    // Validar de la propiedad exista
+    const propiedad = await Propiedad.findByPk(id)
+
+    if(!propiedad){
+        return res.redirect('/mis-propiedades')
+    }
+
+    // Validad que la propiedad no este publicada
+    if (propiedad.publicado){
+        return res.redirect('/mis-propiedades')
+    }
+
+    // Validar que la propiedad pertenece a quien visita esta pagina
+    if( req.usuario.id.toString() !== propiedad.usuarioId.toString()){
+        return res.redirect('/mis-propiedades')
+    }
+
     res.render('propiedades/agregar-imagen',{
         pagina: 'Agregar Imagen'
     })
--- seed/usuarios.js
+++ seed/usuarios.js
@@ -6,7 +6,13 @@ const usuarios = [
         email: 'juako.r@gmail.com',
         confirmado: 1,
         password: bcrypt.hashSync('Juako90', 10)
-    }
+    },
+    {
+        nombre: 'Kiam',
+        email: 'miyudanger20@gmail.com',
+        confirmado: 1,
+        password: bcrypt.hashSync('Kiam90', 10)
+    },
 ]
 
 export default usuarios
